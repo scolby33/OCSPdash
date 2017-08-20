360bcf08f589d731d247cbe7eb5741e4a58ad263
Scott Colby <scolby33@gmail.com>
2017-08-20 03:36:25 -0700

    Update the models a bit

--- src/ocspdash/web/models.py
+++ src/ocspdash/web/models.py
@@ -14,6 +14,7 @@ class OCSPResponderStatus(Enum):
 class Authority(object):
     name: str = None
     number_of_certs: int = None
+    rank: int = None  # update this when rankings change. Don't delete formerly-high-ranked authorities as that would mess up relations to old test results
 
 
 class OCSPResponder(object):
@@ -22,12 +23,16 @@ class OCSPResponder(object):
     authority: Authority = None  # this should be a relation
     number_of_certs: int = None
     subject_cert: Certificate = None  # cached, a relation
+    current: bool = None  # whether there are any valid certs that use this URL, can be null if we haven't checked yet
+    # if current is False, display the row with strikethrough/muted colors/something
 
 
 class Certificate(object):
     subject_cert: bytes = None
     issuer_cert: bytes = None
     retrieved_on: datetime = None  # expire the cached certificate when this date is more than 7 days ago
+    ocsp_url: OCSPResponder = None  # a relation
+    authority: Authority = None  # a relation, maybe not necessary since we can walk up via the associated OCSPResponder
 
     @property
     def expires_on(self) -> datetime:  # expire the cached certificate when this date is in the past
@@ -40,12 +45,11 @@ class TestResult(object):
     url: OCSPResponder = None  # a relation, this name might not need to be "url"?
     timestamp: datetime = None  # when the test was run
     location: str = None  # where we got this result from
-    current: bool = None  # whether there are any valid certs that use this URL
     ping: bool = None  # did the server respond to a ping?
     ocsp: bool = None  # did a valid OCSP request get a good response?
 
     @property
-    def status(self) -> OCSPResponderStatus:
+    def status(self) -> OCSPResponderStatus:  # relates to the glyphicon displayed
         if self.ocsp and self.ping:
             return OCSPResponderStatus.good
         elif self.ocsp and not self.ping:
