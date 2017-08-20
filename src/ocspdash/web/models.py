3580e26d10aa336282892bcc07c46ae9a57ea926
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-20 16:20:01 +0200

    Make concept into models
    @scolby33 I think I may be misunderstanding the business logic and made
    some mistakes in translating this to database models. Please advise

--- src/ocspdash/web/models.py
+++ src/ocspdash/web/models.py
@@ -1,7 +1,24 @@
+# -*- coding: utf-8 -*-
+
 from datetime import datetime
 from enum import Enum
 
 from oscrypto import asymmetric
+from sqlalchemy import (
+    Binary,
+    Column,
+    ForeignKey,
+    Boolean,
+    UniqueConstraint,
+    Integer,
+    String,
+    DateTime,
+    Text
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
 
 
 class OCSPResponderStatus(Enum):
@@ -11,28 +28,64 @@ class OCSPResponderStatus(Enum):
     unknown = 'unknown'
 
 
-class Authority(object):
-    name: str = None
-    number_of_certs: int = None
-    rank: int = None  # update this when rankings change. Don't delete formerly-high-ranked authorities as that would mess up relations to old test results
+class Authority(Base):
+    """Represents the authority that issues certificates"""
+    __tablename__ = 'authority'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(255), nullable=False, index=True, doc='The name of the authority')
+
+    number_of_certs = Column(Integer)  # TODO document this
+
+    rank = Column(Integer, doc=("Update this when rankings change. Don't delete formerly-high-ranked "
+                                "authorities as that would mess up relations to old test results"))
+
+    endpoints = relationship(
+        'Endpoint',
+        secondary=Responder,
+        primaryjoin=(id == Responder.authority_id),
+        secondaryjoin=(id == Responder.endpoint_id),
+        backref=backref('authorities')
+    )
 
 
-class OCSPResponder(object):
-    # NOTE: there may be a case where one OCSP URL is used by more than one authority. URL cannot be considered unique
-    url: str = None
-    authority: Authority = None  # this should be a relation
-    number_of_certs: int = None
-    subject_cert: Certificate = None  # cached, a relation
-    current: bool = None  # whether there are any valid certs that use this URL, can be null if we haven't checked yet
-    # if current is False, display the row with strikethrough/muted colors/something
+class Endpoint(Base):
+    """Represents the URL at which an OCSP is present"""
+    __tablename__ = 'endpoint'
+
+    id = Column(Integer, primary_key=True)
+
+    url = Column(Text, nullable=False, doc='The URL of the OCSP endpoint')
 
 
-class Certificate(object):
-    subject_cert: bytes = None
-    issuer_cert: bytes = None
-    retrieved_on: datetime = None  # expire the cached certificate when this date is more than 7 days ago
-    ocsp_url: OCSPResponder = None  # a relation
-    authority: Authority = None  # a relation, maybe not necessary since we can walk up via the associated OCSPResponder
+class Responder(Base):
+    """Represents the unique pair of authority/endpoint"""
+    __tablename__ = 'responder'
+
+    id = Column(Integer, primary_key=True)
+
+    authority_id = Column(Integer, ForeignKey('authority.id'), nullable=False, doc='The authority')
+    endpoint_id = Column(Integer, ForeignKey('endpoint.id'), nullable=False, doc='The endpoint')
+
+    __table_args__ = (
+        UniqueConstraint(authority_id, endpoint_id),
+    )
+
+
+class Certificate(Base):
+    """Represents a certificate and its origin"""
+    __tablename__ = 'certificate'
+
+    id = Column(Integer, primary_key=True)
+
+    responder_id = Column(Integer, ForeignKey('responder.id'))
+    responder = relationship('Responder')
+
+    subject = Column(Binary, nullable=False, doc="")
+    issuer = Column(Binary, nullable=False, doc="")
+    retrieved = Column(DateTime, nullable=False,
+                       doc="expire the cached certificate when this date is more than 7 days ago")
 
     @property
     def expires_on(self) -> datetime:  # expire the cached certificate when this date is in the past
@@ -40,13 +93,38 @@ class Certificate(object):
         return certificate.asn1['tbs_certificate']['validity']['not_after'].native
 
 
-class TestResult(object):
-    authority: Authority = None  # a relation
-    url: OCSPResponder = None  # a relation, this name might not need to be "url"?
-    timestamp: datetime = None  # when the test was run
-    location: str = None  # where we got this result from
-    ping: bool = None  # did the server respond to a ping?
-    ocsp: bool = None  # did a valid OCSP request get a good response?
+class User(Base):
+    """References a user"""
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+
+    location = Column(Text, doc='The location to be displayed')
+
+
+class Result(Base):
+    """The information about the result from a ping"""
+    __tablename__ = 'result'
+
+    id = Column(Integer, primary_key=True)
+
+    responder_id = Column(Integer, ForeignKey('responder.id'))
+    responder = relationship('Responder')
+
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, doc='The user that ran the test')
+    user = relationship('User')
+
+    retrieved = Column(DateTime, default=datetime.utcnow, doc="When was the test run")
+    ping = Column(Boolean, default=False, nullable=False, doc='did the server respond to a ping?')
+    ocsp = Column(Boolean, default=False, nullable=False, doc='did a valid OCSP request get a good response?')
+
+    @property
+    def authority(self):
+        return self.responder.authority
+
+    @property
+    def endpoint(self):
+        return self.responder.endpoint
 
     @property
     def status(self) -> OCSPResponderStatus:  # relates to the glyphicon displayed
