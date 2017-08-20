5305a3c692bd874111811606692007515571a9aa
Scott Colby <scolby33@gmail.com>
2017-08-20 11:30:52 -0700

    Model changes to improve documentation and clarify a few classes.

--- src/ocspdash/web/models.py
+++ src/ocspdash/web/models.py
@@ -34,9 +34,10 @@ class Authority(Base):
 
     id = Column(Integer, primary_key=True)
 
-    name = Column(String(255), nullable=False, index=True, doc='The name of the authority')
+    name = Column(String(255), nullable=False, index=True, doc='the name of the authority')
 
-    number_of_certs = Column(Integer)  # TODO document this
+    cardinality = Column(Integer, doc="The number of certs observed from this authority in the wild. Update this "
+                                      "when rankings change.")
 
     rank = Column(Integer, doc=("Update this when rankings change. Don't delete formerly-high-ranked "
                                 "authorities as that would mess up relations to old test results"))
@@ -51,12 +52,12 @@ class Authority(Base):
 
 
 class Endpoint(Base):
-    """Represents the URL at which an OCSP is present"""
+    """Represents the URL at which an OCSP responder is present"""
     __tablename__ = 'endpoint'
 
     id = Column(Integer, primary_key=True)
 
-    url = Column(Text, nullable=False, doc='The URL of the OCSP endpoint')
+    url = Column(Text, nullable=False, doc='the URL of the OCSP endpoint')
 
 
 class Responder(Base):
@@ -65,27 +66,30 @@ class Responder(Base):
 
     id = Column(Integer, primary_key=True)
 
-    authority_id = Column(Integer, ForeignKey('authority.id'), nullable=False, doc='The authority')
-    endpoint_id = Column(Integer, ForeignKey('endpoint.id'), nullable=False, doc='The endpoint')
+    authority_id = Column(Integer, ForeignKey('authority.id'), nullable=False, doc='the authority')
+    endpoint_id = Column(Integer, ForeignKey('endpoint.id'), nullable=False, doc='the endpoint')
+
+    cardinality = Column(Integer, doc="The number of certs observed using this authority/endpoint pair in the "
+                                      "wild. Update this when rankings are updated.")
 
     __table_args__ = (
         UniqueConstraint(authority_id, endpoint_id),
     )
 
 
-class Certificate(Base):
-    """Represents a certificate and its origin"""
-    __tablename__ = 'certificate'
+class Chain(Base):
+    """Represents a certificate and its issuing certificate"""
+    __tablename__ = 'chain'
 
     id = Column(Integer, primary_key=True)
 
     responder_id = Column(Integer, ForeignKey('responder.id'))
     responder = relationship('Responder')
 
-    subject = Column(Binary, nullable=False, doc="")
-    issuer = Column(Binary, nullable=False, doc="")
+    subject = Column(Binary, nullable=False, doc='raw bytes of the subject certificate')
+    issuer = Column(Binary, nullable=False, doc="raw bytes of the subject's issuer certificate")
     retrieved = Column(DateTime, nullable=False,
-                       doc="expire the cached certificate when this date is more than 7 days ago")
+                       doc='expire the cached chain when this date is more than 7 days ago')
 
     @property
     def expires_on(self) -> datetime:  # expire the cached certificate when this date is in the past
@@ -99,7 +103,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
 
-    location = Column(Text, doc='The location to be displayed')
+    location = Column(Text, doc='the location to be displayed')
 
 
 class Result(Base):
@@ -111,10 +115,12 @@ class Result(Base):
     responder_id = Column(Integer, ForeignKey('responder.id'))
     responder = relationship('Responder')
 
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, doc='The user that ran the test')
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False, doc='the user that ran the test')
     user = relationship('User')
 
-    retrieved = Column(DateTime, default=datetime.utcnow, doc="When was the test run")
+    certificate: Chain = None  # TODO: the certificate used to make the OCSP request
+
+    retrieved = Column(DateTime, default=datetime.utcnow, doc='when the test was run')
     ping = Column(Boolean, default=False, nullable=False, doc='did the server respond to a ping?')
     ocsp = Column(Boolean, default=False, nullable=False, doc='did a valid OCSP request get a good response?')
 
