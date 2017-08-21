0a2b27048b367de8daad89807083a0a3b07aa2fc
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-21 23:16:45 +0200

    Add JSON endpoints in web app

--- src/ocspdash/web/app.py
+++ src/ocspdash/web/app.py
@@ -142,9 +142,45 @@ def get_payload():
     return jsonify(example_payload)
 
 
+@api.route('/recent')
+def get_recent():
+    result = current_app.manager.get_most_recent_result_for_each_location()
+    return render_template('recent.html', payload=result)
+
+
+@api.route('/authority')
+def get_authorities():
+    return jsonify([
+        authority.to_json()
+        for authority in current_app.manager.session.query(Authority).all()
+    ])
+
+
+@api.route('/authority/<int:authority_id>')
+def get_authority(authority_id):
+    return jsonify(current_app.manager.session.query(Authority).get(authority_id).to_json())
+
+
 @api.route('/responder/<int:responder_id>')
 def get_responder(responder_id):
     return jsonify(current_app.manager.session.query(Responder).get(responder_id).to_json())
+
+
+@api.route('/responder/<int:responder_id>/chain')
+def get_responder_chains(responder_id):
+    return jsonify([
+        chain.to_json()
+        for chain in current_app.manager.session.query(Responder).get(responder_id).chains
+    ])
+
+
+@api.route('/responder/<int:responder_id>/result')
+def get_responder_results(responder_id):
+    return jsonify([
+        result.to_json()
+        for chain in current_app.manager.session.query(Responder).get(responder_id).chains
+        for result in chain.results
+    ])
 
 
 @api.route('/responder')
