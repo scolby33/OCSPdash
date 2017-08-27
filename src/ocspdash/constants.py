1b75e25746305ec47a72874e854744c395cceec6
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-08-27 20:47:45 +0200

    Allow config to be set from environment

--- src/ocspdash/constants.py
+++ src/ocspdash/constants.py
@@ -6,14 +6,13 @@ from . import __name__, __version__
 
 OCSPDASH_API_VERSION = 'v0'
 
-OCSPDASH_DIRECTORY = os.path.join(os.path.expanduser('~'), '.ocspdash')
+OCSPDASH_DIRECTORY = os.environ.get('OCSPDASH_DIRECTORY', os.path.join(os.path.expanduser('~'), '.ocspdash'))
 
 if not os.path.exists(OCSPDASH_DIRECTORY):
     os.makedirs(OCSPDASH_DIRECTORY)
 
-OCSPDASH_DATABASE_PATH = os.path.join(OCSPDASH_DIRECTORY, 'ocspdash.db')
-OCSPDASH_DATABASE_CONNECTION = 'sqlite:///' + OCSPDASH_DATABASE_PATH
+OCSPDASH_DATABASE_CONNECTION = 'sqlite:///' + os.path.join(OCSPDASH_DIRECTORY, 'ocspdash.db')
 
-CENSYS_RATE_LIMIT = 0.2  # max requests per second
+CENSYS_RATE_LIMIT = float(os.environ.get('OCSPDASH_RATE', 0.2))  # max requests per second
 
 OCSPDASH_USER_AGENT = ' '.join([requests.utils.default_user_agent(), f'{__name__}/{__version__}'])
