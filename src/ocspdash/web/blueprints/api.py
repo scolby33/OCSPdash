2053e9937887aae4b8f582a265d2717ba6b20158
Scott Colby <scolby33@gmail.com>
2018-05-11 03:20:17 -0700

    Comment out the generic "get" methods that I don't want to expose quite yet

--- src/ocspdash/web/blueprints/api.py
+++ src/ocspdash/web/blueprints/api.py
@@ -5,76 +5,9 @@ from hmac import compare_digest
 
 from flask import Blueprint, jsonify, current_app, request, abort
 
-from ocspdash.models import Authority, Responder
+# from ocspdash.models import Authority, Responder
 
 api = Blueprint('api', __name__)
-
-
-@api.route('/status')
-def get_payload():
-    """Spits back the current payload"""
-    payload = current_app.manager.make_payload()
-
-    return jsonify(payload)
-
-
-@api.route('/recent')
-def get_recent():
-    """Get the most recent result set"""
-    result = current_app.manager.get_most_recent_result_for_each_location()
-
-    return jsonify(result)
-
-
-@api.route('/authority')
-def get_authorities():
-    return jsonify([
-        authority.to_json()
-        for authority in current_app.manager.session.query(Authority).all()
-    ])
-
-
-@api.route('/authority/<int:authority_id>')
-def get_authority(authority_id):
-    authority = current_app.manager.get_authority_by_id(authority_id)
-
-    return jsonify(authority.to_json())
-
-
-@api.route('/responder')
-def get_responders():
-    return jsonify([
-        responder.to_json()
-        for responder in current_app.manager.session.query(Responder).all()
-    ])
-
-
-@api.route('/responder/<int:responder_id>')
-def get_responder(responder_id):
-    responder = current_app.manager.get_responder_by_id(responder_id)
-
-    return jsonify(responder.to_json())
-
-
-@api.route('/responder/<int:responder_id>/chain')
-def get_responder_chains(responder_id):
-    responder = current_app.manager.get_responder_by_id(responder_id)
-
-    return jsonify([
-        chain.to_json()
-        for chain in responder.chains
-    ])
-
-
-@api.route('/responder/<int:responder_id>/result')
-def get_responder_results(responder_id):
-    responder = current_app.manager.get_responder_by_id(responder_id)
-
-    return jsonify([
-        result.to_json()
-        for chain in responder.chains
-        for result in chain.results
-    ])
 
 
 @api.route('/register', methods=['POST'])
@@ -93,3 +26,69 @@ def register_location_key():
     current_app.manager.session.commit()
 
     return jsonify(location.to_json())
+
+# @api.route('/status')
+# def get_payload():
+#     """Spits back the current payload"""
+#     payload = current_app.manager.make_payload()
+#
+#     return jsonify(payload)
+
+
+# @api.route('/recent')
+# def get_recent():
+#     """Get the most recent result set"""
+#     result = current_app.manager.get_most_recent_result_for_each_location()
+#
+#     return jsonify(result)
+
+
+# @api.route('/authority')
+# def get_authorities():
+#     return jsonify([
+#         authority.to_json()
+#         for authority in current_app.manager.session.query(Authority).all()
+#     ])
+
+
+# @api.route('/authority/<int:authority_id>')
+# def get_authority(authority_id):
+#     authority = current_app.manager.get_authority_by_id(authority_id)
+#
+#     return jsonify(authority.to_json())
+
+
+# @api.route('/responder')
+# def get_responders():
+#     return jsonify([
+#         responder.to_json()
+#         for responder in current_app.manager.session.query(Responder).all()
+#     ])
+
+
+# @api.route('/responder/<int:responder_id>')
+# def get_responder(responder_id):
+#     responder = current_app.manager.get_responder_by_id(responder_id)
+#
+#     return jsonify(responder.to_json())
+
+
+# @api.route('/responder/<int:responder_id>/chain')
+# def get_responder_chains(responder_id):
+#     responder = current_app.manager.get_responder_by_id(responder_id)
+#
+#     return jsonify([
+#         chain.to_json()
+#         for chain in responder.chains
+#     ])
+
+
+# @api.route('/responder/<int:responder_id>/result')
+# def get_responder_results(responder_id):
+#     responder = current_app.manager.get_responder_by_id(responder_id)
+#
+#     return jsonify([
+#         result.to_json()
+#         for chain in responder.chains
+#         for result in chain.results
+#     ])
