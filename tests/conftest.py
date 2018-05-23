d8dddf8f9881fbea01fcc18b25d9c57932e0110e
Scott Colby <scolby33@gmail.com>
2018-05-22 17:00:44 -0700

    An alternate manager revision
    @cthoyt

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,18 +12,18 @@ def app():
 def manager(tmpdir_factory):
     db_path = tmpdir_factory.mktemp('ocspdash').join('ocspdash.db')
 
-    return Manager(connection=f'sqlite:///{db_path}', echo=True)
+    return Manager.from_args(connection=f'sqlite:///{db_path}', echo=True)
 
 
 @pytest.fixture(scope='function')
 def manager_transaction(manager):
     connection = manager.engine.connect()
     transaction = connection.begin()
-    manager.session_maker.configure(bind=connection)
+    manager.session.configure(bind=connection)
 
     yield manager
 
-    manager.session_maker.configure(bind=manager.engine)
+    manager.session.configure(bind=manager.engine)
     manager.session.remove()
     transaction.rollback()
     connection.close()
@@ -32,11 +32,11 @@ def manager_transaction(manager):
 @pytest.fixture(scope='session')
 def manager_alt(manager):
     manager._connection = manager.engine.connect()
-    manager.session_maker.configure(bind=manager._connection)
+    manager.session.configure(bind=manager._connection)
 
     yield manager
 
-    manager.session_maker.configure(bind=manager.engine)
+    manager.session.configure(bind=manager.engine)
     manager._connection.close()
     del manager._connection
 
