e87cebb0efc97041df13f1140baef7231ad2c598
Scott Colby <scolby33@gmail.com>
2018-05-27 01:32:07 -0700

    Docstring cleanup for web.run

--- src/ocspdash/web/run.py
+++ src/ocspdash/web/run.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-""" This file should be used to run the flask app with something like Gunicorn. For example:
-gunicorn -b 0.0.0.0:8000 ocspdash.web.run:app
+"""This file should be used to run the flask app with something like Gunicorn.
+
+For example: gunicorn -b 0.0.0.0:8000 ocspdash.web.run:app
 
 This file should NOT be imported anywhere, though, since it would instantiate the app.
 """
