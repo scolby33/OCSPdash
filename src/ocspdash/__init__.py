48ccd0f34d5037ebc58465fa3ab5de329533487e
Scott Colby <scolby33@gmail.com>
2018-06-30 00:42:20 -0700

    A declarative setup.py

--- src/ocspdash/__init__.py
+++ src/ocspdash/__init__.py
@@ -2,16 +2,29 @@
 
 """A dashboard for the status of the top certificate authorities' OCSP responders."""
 
-# metadata
-__version__ = '0.1.0-dev'
+from distlib.database import DistributionPath as _DistributionPath
+_distribution = _DistributionPath(include_egg=True).get_distribution('ocspdash')
+if _distribution:
+    _metadata = _distribution.metadata.todict()
 
-__title__ = 'OCSPdash'
-# keep the __description__ synchronized with the package docstring
-__description__ = "A dashboard for the status of the top certificate authorities' OCSP responders."
-__url__ = 'https://github.com/scolby33/OCSPdash'
+    __version__ = _metadata['version']
 
-__author__ = 'Scott Colby'
-__email__ = 'scolby33@gmail.com'
+    __title__ = _metadata['name']
+    # keep the description in setup.cfg synchronized with the package docstring
+    __description__ = _metadata['summary']
+    __url__ = _metadata['home_page']
 
-__license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2017 Scott Colby and Charles Tapley Hoyt'
+    __author__ = _metadata['author']
+    __email__ = _metadata['author_email']
+
+    __license__ = _metadata['license']
+    __copyright__ = 'Copyright (c) 2017 Scott Colby and Charles Tapley Hoyt'
+
+    del _metadata
+else:
+    import warnings
+    warnings.warn('Unable to retrieve distribution metadata--is OCSPdash fully installed?', RuntimeWarning)
+    del warnings
+
+# cleanup the package namespace
+del _DistributionPath, _distribution
