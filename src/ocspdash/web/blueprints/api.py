92b4709bb36f3348959bf183dcc13f437ee3c9c4
Scott Colby <scolby33@gmail.com>
2018-08-07 02:43:09 -0700

    Add more error checking to the API.

--- src/ocspdash/web/blueprints/api.py
+++ src/ocspdash/web/blueprints/api.py
@@ -34,37 +34,36 @@ def register_location_key():
     try:
         unverified_claims = jwt.get_unverified_claims(request.data)
     except jwt.JWTError:
-        return 'malformed JWT', HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, 'malformed JWT')
 
     try:
         unverified_public_key = b64decode(unverified_claims['pk']).decode('utf-8')
     except KeyError:
-        return "'pk' missing from unverified claims", HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, "'pk' missing from claims")
     except (binascii.Error, UnicodeError):
-        abort(400)  # bad data in 'pk' claim
-        return "failed to decode 'pk' claim", HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, "failed to decode 'pk' claim")
 
     try:
         claims = jwt.decode(request.data, unverified_public_key)
     except JWTError:
-        return 'failed to validate JWT', HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, 'failed to validate JWT')
 
     try:
         public_key = claims['pk']
     except KeyError:
-        return "'pk' misisng from verified claims", HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, "'pk' misisng from claims")
 
     try:
         invite_token = b64decode(claims['token'])
     except KeyError:
-        return "'token' missing from verified claims", HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, "'token' missing from claims")
     except binascii.Error:
-        return "failed to decode 'token' claim", HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, "failed to decode 'token' claim")
 
     try:
         manager.process_location(invite_token, public_key)
     except ValueError:
-        return 'bad invite or public key', HTTPStatus.BAD_REQUEST
+        abort(HTTPStatus.BAD_REQUEST, 'bad invite or public key')
 
     return '', HTTPStatus.NO_CONTENT
 
@@ -86,7 +85,7 @@ def get_manifest():
     """
     n = request.args.get('n', type=int, default=10)  # TODO make configurable at app level
     if n > 10:
-        abort(400, 'n too large, max is 10')  # TODO get the max config value here too
+        abort(HTTPStatus.BAD_REQUEST, 'n too large, max is 10')  # TODO get the max config value here too
     manifest_lines = io.StringIO()
     with jsonlines.Writer(manifest_lines, sort_keys=True) as writer:
         writer.write_all(
@@ -101,7 +100,10 @@ def get_manifest():
 
 def _prepare_result_dictionary(result_data):
     certificate_chain_uuid: uuid.UUID = uuid.UUID(result_data['certificate_chain_uuid'])
+
     chain = manager.get_chain_by_certificate_chain_uuid(certificate_chain_uuid)
+    if not chain:
+        raise ValueError(f'No chain with certificate_chain_uuid: {certificate_chain_uuid}')
 
     retrieved = datetime.strptime(result_data['time'], '%Y-%m-%dT%H:%M:%SZ')
 
@@ -121,18 +123,37 @@ def submit():
     tags:
         - ocsp
     """
-    submitted_token_header = jwt.get_unverified_header(request.data)
+    try:
+        submitted_token_header = jwt.get_unverified_header(request.data)
+    except jwt.JWTError:
+        abort(HTTPStatus.BAD_REQUEST, 'malformed JWT')
 
-    key_id = uuid.UUID(submitted_token_header['kid'])
+    try:
+        key_id = uuid.UUID(submitted_token_header['kid'])
+    except KeyError:
+        abort(HTTPStatus.BAD_REQUEST, "'kid' missing from JWT header")
+
     submitting_location = manager.get_location_by_key_id(key_id)
+    if not submitting_location:
+        abort(HTTPStatus.BAD_REQUEST, f'no location with key id: {key_id}')
 
     try:
         claims = jwt.decode(request.data, submitting_location.pubkey.decode('utf-8'))
     except JWTError:
-        return abort(400)
+        abort(HTTPStatus.BAD_REQUEST, 'failed to validate JWT')
 
-    prepared_result_dicts = (_prepare_result_dictionary(result_data)
-                             for result_data in claims[OCSP_RESULTS_JWT_CLAIM])
+    try:
+        results = claims[OCSP_RESULTS_JWT_CLAIM]
+    except KeyError:
+        abort(HTTPStatus.BAD_REQUEST, f"'{OCSP_RESULTS_JWT_CLAIM}' missing from claims")
+
+    try:
+        prepared_result_dicts = (_prepare_result_dictionary(result_data)
+                                 for result_data in results)
+    except (KeyError, ValueError):
+        abort(HTTPStatus.BAD_REQUEST, 'invalid result data')
+
+    # TODO: can this raise an exception? I think yes if there's a constraint broken on the DB when commit is called()
     manager.insert_payload(submitting_location, prepared_result_dicts)
 
-    return ('', HTTPStatus.NO_CONTENT)
+    return '', HTTPStatus.NO_CONTENT
